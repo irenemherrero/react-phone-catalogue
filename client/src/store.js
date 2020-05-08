8631130001c91c002cfdac21615cd1fcc4d2f133
JGLeal <jgleal@gmail.com>
2020-05-08 18:01:42 +0200

    clean store

--- client/src/store.js
+++ client/src/store.js
@@ -1,17 +1,9 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk'
-import { createLogger } from 'redux-logger'
 import rootReducer from './rootReducer'
 
-const loggerMiddleware = createLogger()
-
-const configureStore = (preloadedState) => {
-  return createStore(
-    rootReducer,
-    // phonesReducer,
-    preloadedState,
-    applyMiddleware(thunkMiddleware, loggerMiddleware)
-  )
+const configureStore = () => {
+  return createStore(rootReducer, applyMiddleware(thunkMiddleware))
 }
 
 export { configureStore }
