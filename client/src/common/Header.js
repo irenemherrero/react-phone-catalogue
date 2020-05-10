a6c7ae3da9272c53294130a6a91068afaa714504
JGLeal <jgleal@gmail.com>
2020-05-10 12:34:15 +0200

    common header

--- client/src/common/Header.js
+++ client/src/common/Header.js
@@ -1,29 +1,42 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import tw from 'tailwind.macro'
-import logo from '../assets/logo.png'
+import logo from './../assets/logo.png'
 
-const StickyHeader = styled.div`
-  ${tw`flex sticky top-0 bg-gray-600 p-3 z-50`}
+const StickyHeader = styled.header`
+  ${tw`flex sticky top-0 bg-gray-600 p-3 z-50 text-white`}
 `
 const Logo = styled.div`
-  ${tw`flex items-center text-white mr-12`}
+  ${tw`flex items-center`}
   img {
     ${tw`w-10 mr-4`}
   }
-  h1 {
-    ${tw`font-semibold text-lg`}
+  span {
+    ${tw`tracking-wide font-semibold text-lg`}
   }
 `
-const Header = () => {
+const Title = styled.div`
+  ${tw`w-full absolute text-center font-semibold tracking-wider text-2xl`}
+`
+const Header = (props) => {
+  const { title } = props
   return (
     <StickyHeader>
       <Logo>
         <img src={logo} />
-        <h1>JGL Phones</h1>
+        <span>JGL Phones</span>
       </Logo>
+      <Title>{title}</Title>
     </StickyHeader>
   )
+}
+
+Header.propTypes = {
+  title: PropTypes.string,
+}
+Header.defaultProps = {
+  title: '',
 }
 
 export { Header }
