9cb6d86badac2d2b310c2de85c9f16da1662ccc3
JGLeal <jgleal@gmail.com>
2020-05-11 19:37:38 +0200

    dockerize

--- client/src/common/Header/index.js
+++ client/src/common/Header/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import logo from '@/assets/logo.png'
 import { StickyHeader, Logo, Title } from './styles'
 
 const Header = (props) => {
